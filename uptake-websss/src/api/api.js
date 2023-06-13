d01c274a02746e30dea0e257be7d0a54caf0a9fc
KevinNeel <kevinneel007@gmail.com>
2023-06-13 17:30:32 +0530

    inserted the route

--- uptake-websss/src/api/api.js
+++ uptake-websss/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL:'http://localhost:8000'});
+const API = axios.create({baseURL:'http://13.53.130.121:8000'});
 
 export const sendMail = (mailData)=> API.post('/', mailData);
 
