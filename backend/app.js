6b5d3c8268727ed15a77442a8d52167c362e8a78
KevinNeel <kevinneel007@gmail.com>
2023-06-13 18:40:42 +0530

    made some changes

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,16 @@ dotenv.config();
 
 import cors from 'cors';
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 import { sendMail } from './function/sendMail.js';
 
 app.post('/', sendMail)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running at port:${PORT}`);
 })
--- uptake-websss/src/api/api.js
+++ uptake-websss/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL:'http://13.53.130.121:8000'});
+const API = axios.create({baseURL:'http://localhost:8000'});
 
-export const sendMail = (mailData)=> API.post('http://13.51.70.251:8000/', mailData);
+export const sendMail = (mailData)=> API.post(`${API}/`, mailData);
 
