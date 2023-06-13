a83709c9da285f3771f728f81f58b10b732c334b
KevinNeel <kevinneel007@gmail.com>
2023-06-13 18:49:24 +0530

    made changes in api

--- uptake-websss/src/api/api.js
+++ uptake-websss/src/api/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const API = axios.create({baseURL:'http://localhost:8000'});
 
-export const sendMail = (mailData)=> API.post(`/`, mailData);
+export const sendMail = (mailData)=> API.post('/', mailData);
 
